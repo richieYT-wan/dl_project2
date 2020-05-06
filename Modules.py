5927255da3e67cf8e950e6edc01af5932d81ae1d
Richie Wan <richie.wan@epfl.ch>
2020-05-06 11:35:48 +0200

    CHANGED SHIT

--- Modules.py
+++ Modules.py
@@ -4,17 +4,16 @@ import torch
 class Module() :
     def __init__(self):
         pass
+    
     def forward(self, *input):
         raise NotImplementedError
+    
     def backward(self,*gradwrtoutput):
         raise NotImplementedError
-    def param(self):
-        return []
-    def zero_grad(self):
-        pass 
-    #From stackoverflow : Must use pass to have an empty statement otherwise problem.
-    #Is it really useful to have it in Module? I think this is only used by Linear.
     
+    def param(self):
+        pass
+        
     def __call__(self, *input):
         return self.forward(*input)
     
@@ -38,20 +37,11 @@ class Linear(Module):
         self.db = torch.zeros(out_dim)
     
     def param(self):
-        return [self.w,self.dw,self.b,self.db]
-
-    def zero_grad(self):
-        """
-            Function to set grad to zero. Used in gradient step
-        """
-        self.dw.zero_()
-        self.db.zero_()
+        return [[self.w, self.dw], [self.b, self.db]]
         
-   
-
     def forward(self,x):
         """
-            results of FW pass of layer, returns s = w*x + b
+            results of FW pass of layer, returns s = x*w + b
             using formula from Practical 03
         """
         s = x.mm(self.w)+self.b
@@ -62,29 +52,78 @@ class Linear(Module):
             Backward pass, see practical 03 and slide 9,11 of course 3.6
             Accumulates gradient according to chain rule, with mat product.
             dl_ds is the derivative of loss wrt to s = x*w + b
-            dl_ds = dl_dx.mul(sigma'(s)) which is given to him by the
+            dl_ds = dl_dx.mul(sigma'(s)) which is given to him the
             backward of activation functions.
-            
-            returns dl_dx_prev which the the derivative of the loss
-            wrt to x of the previous layer
         """
-        #ex : x = size mxn, dl_ds = size nxk
-        #(m x n) * (n x k) = (m x k)
-        #(25 x 4) * (4 * 2)  = (25 x 2)
-        #x size : 4x25, x.t() size = 25x4
-        #dl_ds size = 4x2 --> x.t()*dl_ds
-        
-        dl_dx_prev = self.w.mm(dl_ds.t())
-        #print("ASDFOJASDIFJS",dl_dx_prev)
-        #print("niquetamère",dl_ds.size())
-        #print("X ta race",x.size())
-        #print("X transpo",x.t().size())
-        #print(self.dw.size())
-        #print(self.db.size())
-        self.dw.add_(x.t().mm(dl_ds))
-        self.db.add_(dl_ds)
-        
+        self.dw.add_(dl_ds.view(-1, 1).mm(x.view(1, -1)))
+        self.db.add_(dl_ds.view(-1))
+        dl_dx_prev = self.w.t().mm(dl_ds)
         return dl_dx_prev
+    
+    def zero_grad(self):
+        """
+            Function to set grad to zero. Used in gradient step
+        """
+        self.dw.zero_()
+        self.db.zero_()
+        
+class Sequential(Module):
+    """
+        An instance of the sequential class contains the Modules passed to it as arguments.
+        For example, pass linear, relu to it, it will be stored within its attribute "members"
+        the attribute memory is used to save the operations in the order in which they were done
+        to allow backprop (?)
+    """
+    def __init__(self,*Modules):
+        super(Sequential, self).__init__()
+        self.members = Modules
+        
+        self.parameters = []
+        for module in self.members:
+            if module.param() is not None:
+                for p in module.param():
+                    self.parameters.append(p)
+        
+        self.memory = []
+        self.lossmemory = []
+        
+    def param(self):
+        
+        return self.parameters
+    
+    def forward(self,x):
+        #MUST ADD THINGS TO MEMORY AFTER OPERATION.
+        # x is the input to which the operations of each module must be applied in the right order.
+        #Does this need to save the first entry ?
+        #It should save the value at each step (ex what is x1, s1, sigma(s1), etc.)
+        #this is done in the 
+        #Should backprop also have another memory to save the backprop'd gradient and what else? Loss??
+        
+        for module in self.members:
+            x = module(x)
+            self.memory.append(x)
+        return x
+        
+    def backward(self, out, dl):
+        #must enumerate backwards in the list in memory. 
+        #what should this do ?
+        #should save all the losses at each operation?
+        #backward (from Module) takes as argument : Z, dloss, where z is a
+        #can use for reversed(...) or use for i in () then use memory[-1-i] to iterate backward.
+        self.lossmemory.append(dl)
+        for idx, module in reversed(enumerate(self.members)):
+            #the input for backward is the value saved in memory 
+            dl = module.backward(self.memory[i],dl)
+            self.lossmemory.append(dl)
+        #reversed loss memory
+        return self.lossmemory[::-1]
+    
+    def zero_grad(self):
+        #should only do zero grad for linear modules. 
+        #If called on activation modules (like tanh), that does not have it specified,
+        #it should call the function definition from the mother class in which case it will pass.
+        for module in self.members :
+            module.zero_grad()
         
 #ACTIVATION FUNCTIONS
 class ReLu(Module):
@@ -103,24 +142,7 @@ class ReLu(Module):
             not sure about dimensions.....
         """
         return dl_dx * (s>0).float()
-        
-
-class Tanh(Module):
-    def __init__(self):
-        super(Tanh, self).__init__()
-        
-    def forward(self,x):
-        return x.tanh()
     
-    def backward(self,s,dl_dx):
-        """ 
-            dtanh = 1/cosh^2
-            dl_ds = dl_dx * dsigma(s)
-        """
-        return dl_dx* (1/(torch.pow(torch.cosh(s),2)))
-        
-        
-#LOSS         
 class MSE(Module):
     """ SEE PRACTICAL 03 UPDATE DOC LATER"""
     def __init__(self):
@@ -132,45 +154,56 @@ class MSE(Module):
     
     def backward(self, x, t):
         #This is dl_dx. (dloss wrt to x for MSE loss is 2(x-t))
-        t = convert_to_one_hot_labels(torch.tensor([0, 1]), t)
         return 2 * (x - t)
     # MAYBEWRONG UPDATE THIS LATER
-    
-class CrossEntropyLoss(Module):        
-    def __init__(self):
-        Module.__init__(self)
         
-    def softmax(x):
-        """
-            Computes softmax with shift to be numerically stable for
-            large numbers or floats takes exp(x-max(x)) instead of exp(x)
-        """
-            #this is really stablesoftmax(x)
-            #rather than softamx(x)
-        z = x- x.max()
-        exps = torch.exp(z)
-        return (exps/torch.sum(exps))
-    
-    def forward(self,x,t):
-        """
-            With pj = exp(aj)/sum(exp(ak))
-            Loss = -Sum_j (yj) log(pj), 
-            with t the target being the y in the formula
-            and pj = softmax(x)_j
-            log(p)*t does the element-wise product then we sum
-        """
-        p = self.softmax(x)
-        sumResult = -torch.sum(torch.log(p)*t)
-        return sumResult
-    
-    def backward(self,x,t):
-        """
-            computes dLoss 
-            dl/dx_i = pi-yi from the slides
-        """
-        p = self.softmax(x)
-        return p-t
-    
+#class Tanh(Module):
+#    def __init__(self):
+#        super(Tanh, self).__init__()
+#        
+#    def forward(self,x):
+#        return x.tanh()
+#    
+#    def backward(self,s,dl_dx):
+#        """ dtanh = 1/cosh^2"""
+#        return dl_dx* (1/(torch.pow(torch.cosh(s),2)))   
+#               
+#    
+#class CrossEntropyLoss(Module):        
+#    def __init__(self):
+#        Module.__init__(self)
+#        
+#    def softmax(x):
+#        """
+#            Computes softmax with shift to be numerically stable for
+#            large numbers or floats takes exp(x-max(x)) instead of exp(x)
+#        """
+#            #this is really stablesoftmax(x)
+#            #rather than softamx(x)
+#        z = x- x.max()
+#        exps = torch.exp(z)
+#        return (exps/torch.sum(exps))
+#    
+#    def forward(self,x,t):
+#        """
+#            With pj = exp(aj)/sum(exp(ak))
+#            Loss = -Sum_j (yj) log(pj), 
+#            with t the target being the y in the formula
+#            and pj = softmax(x)_j
+#            log(p)*t does the element-wise product then we sum
+#        """
+#        p = self.softmax(x)
+#        sumResult = -torch.sum(torch.log(p)*t)
+#        return sumResult
+#    
+#    def backward(self,x,t):
+#        """
+#            computes dLoss 
+#            dl/dx_i = pi-yi from the slides
+#        """
+#        p = self.softmax(x)
+#        return p-t
+#    
 def convert_to_one_hot_labels(input, target):
     tmp = input.new_zeros(target.size(0), target.max() + 1)
     tmp.scatter_(1, target.view(-1, 1), 1.0)
---
+++ Optimizer.py
@@ -0,0 +1,47 @@
+import math
+import torch
+
+class Optimizer():
+    def __init__(self):
+        pass
+    
+    def step(self):
+        raise NotImplementedError
+    
+class SGD(Optimizer):
+    def __init__(self, model_parameters, eta= 1e-1):
+        super(SGD, self).__init__()
+        """
+            Model parameters to be optimized over (i.e. w, dw, b, db)
+            lr the learning rate to be passed as argument, 1e-1 by default
+            No momentum
+        """
+        self.parameters = model_parameters
+        self.eta = eta
+        
+    def step(self):
+        """
+            In param, we have p[0]= w, p[1] = dw
+            p[2] = b, p[3] = db then we need to update par[0] = par[0]-eta*par[1],
+            i.e. w(t+1) = w(t) - eta * dl_dw
+        """
+        for p in self.parameters: 
+            p[0] = p[0] - self.eta * p[1]
+            
+#class SGD_momentum(Optimizer):
+#    
+#    def __init__(self,model_parameters,eta=1e-1,gamma = 0.5):
+#        self.parameters = model_parameters
+#        self.eta = eta
+#        self.gamma = gamma
+#        #Initially, for t = 0, the momentum is zero as it has not moved yet.
+#        #then at each step, "previous" will be updated.
+#        self.previous = torch.zeros(self.parameters[0].size())
+#    
+#    def step(self):
+#        for par in self.parameters:
+#            difference = (self.eta*par[1]-self.previous)
+#            par[0] = par[0] - self.eta*par[1]-gamma*self.previous
+#            
+#            #this saves the step of this move in memory to be re-used at the next step for momentum.
+#            self.previous = difference
