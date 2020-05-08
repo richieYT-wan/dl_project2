65fc91b1cf9c84540beb108e66d5b784330843ef
pauhsg <48205721+pauhsg@users.noreply.github.com>
2020-05-08 12:18:31 +0200

    Add files via upload

--- Modules.py
+++ Modules.py
@@ -13,6 +13,9 @@ class Module() :
     
     def param(self):
         pass
+    
+    def zero_grad(self):
+        pass
         
     def __call__(self, *input):
         return self.forward(*input)
@@ -37,9 +40,6 @@ class Linear(Module):
         self.db = torch.zeros(out_dim)
         self.current = torch.empty(1,1)
     
-    def param(self):
-        return [[self.w, self.dw], [self.b, self.db]]
-        
     def forward(self,x):
         """
             results of FW pass of layer, returns s = x*w + b
@@ -47,31 +47,17 @@ class Linear(Module):
         """
         self.current = x
         return x.mm(self.w)+self.b
-    
-    #def backward(self, x, dl_ds):
-    #    """
-    #        Backward pass, see practical 03 and slide 9,11 of course 3.6
-    #        Accumulates gradient according to chain rule, with mat product.
-    #        dl_ds is the derivative of loss wrt to s = x*w + b
-    #        dl_ds = dl_dx.mul(sigma'(s)) which is given to him the
-    #        backward of activation functions.
-    #    """
-    #    self.dw.add_(dl_ds.view(-1, 1).mm(x.view(1, -1)))
-    #    self.db.add_(dl_ds.view(-1))
-    #    dl_dx_prev = self.w.t().mm(dl_ds)
-    #    return dl_dx_prev
-   
+
+    '''
     def backward(self, dl_ds):
-        print('linear bw: dl_ds', dl_ds.size())
-        print('linear bw: selfcurrent', self.current.size())
-        #self.dw = dl_ds.mm(self.current.view(1, -1))
-        #self.dw = dl_ds.t().mm(self.current)
         self.dw = self.current.t().mm(dl_ds)
-        print('linear bw: computed dw', self.dw.size())
+        self.db = dl_ds.mean(0) #This is probably wrong actually but it works xd
+        return dl_ds.mm(self.w.t())
+    '''
+
+    def backward(self, dl_ds):
+        self.dw = self.current.t().mm(dl_ds)
         self.db = dl_ds
-        print('linear bw: db', self.db.size())
-        #return self.w.t().mm(dl_ds)
-        print('linear bw: w', self.w.size())
         return dl_ds.mm(self.w.t())
     
     def zero_grad(self):
@@ -80,68 +66,12 @@ class Linear(Module):
         """
         self.dw.zero_()
         self.db.zero_()
-        
-class Sequential(Module):
-    """
-        An instance of the sequential class contains the Modules passed to it as arguments.
-        For example, pass linear, relu to it, it will be stored within its attribute "members"
-        the attribute memory is used to save the operations in the order in which they were done
-        to allow backprop (?)
-    """
-    def __init__(self,*Modules):
-        super(Sequential, self).__init__()
-        self.members = Modules
-        
-        self.parameters = []
-        for module in self.members:
-            if module.param() is not None:
-                for p in module.param():
-                    self.parameters.append(p)
-        
-        self.memory = []
-        self.lossmemory = []
-        
+    
     def param(self):
-        return self.parameters
-    
-    def forward(self, x):
-        #MUST ADD THINGS TO MEMORY AFTER OPERATION.
-        # x is the input to which the operations of each module must be applied in the right order.
-        #Does this need to save the first entry ?
-        #It should save the value at each step (ex what is x1, s1, sigma(s1), etc.)
-        #this is done in the 
-        #Should backprop also have another memory to save the backprop'd gradient and what else? Loss??
-        
-        for module in self.members:
-            x = module(x)
-            self.memory.append(x)
-        return x
-        
-    #def backward(self, out, dl):
-    #    #must enumerate backwards in the list in memory. 
-    #    #what should this do ?
-    #    #should save all the losses at each operation?
-    #    #backward (from Module) takes as argument : Z, dloss, where z is a
-    #    #can use for reversed(...) or use for i in () then use memory[-1-i] to iterate backward.
-    #    self.lossmemory.append(dl)
-    #    for idx, module in reversed(enumerate(self.members)):
-    #        #the input for backward is the value saved in memory 
-    #        dl = module.backward(self.memory[i],dl)
-    #        self.lossmemory.append(dl)
-    #    #reversed loss memory
-    #    return self.lossmemory[::-1]
-    
-    def backward(self, dl_dx):
-        tmp = dl_dx
-        for module in reversed(self.members):
-            tmp = module.backward(tmp)
-    
-    def zero_grad(self):
-        #should only do zero grad for linear modules. 
-        #If called on activation modules (like tanh), that does not have it specified,
-        #it should call the function definition from the mother class in which case it will pass.
-        for module in self.members :
-            module.zero_grad()
+        #print("ARE THESE ZEROS --------------------- \n",self.dw)
+        return [[self.w, self.dw], [self.b, self.db]]
+            
+
         
 #ACTIVATION FUNCTIONS
 class ReLu(Module):
@@ -149,9 +79,9 @@ class ReLu(Module):
         super(ReLu, self).__init__()
         self.current = torch.empty(1,1)
     
-    def forward(self, x):
-        self.current = x
-        return torch.max(x,torch.zeros(x.size()))
+    def forward(self, s):
+        self.current = s
+        return torch.max(s,torch.zeros(s.size()))
     
     def backward(self, dl_dx):
         """
@@ -159,12 +89,8 @@ class ReLu(Module):
             dsigma for relu is f : (1, x>0, 
                                     0, x<0)
             this should work as it gives a logical tensor then into float
-            not sure about dimensions.....
         """
-        #return dl_dx*(self.current>0).view(-1,1).long()
-        print('relu bw: dldx', dl_dx.size())
-        print('relu bw: selfcurrent', self.current.size())
-        print('relu bw: dl_ds', (dl_dx*(self.current>0).float()).size())
+        
         return dl_dx*(self.current>0).float()
     
 class MSE(Module):
@@ -172,62 +98,65 @@ class MSE(Module):
     def __init__(self):
         super(MSE, self).__init__()
     
-    def forward(self, x, t):
+    def forward(self,x,t):
         t = convert_to_one_hot_labels(torch.tensor([0, 1]), t)
         return (x - t).pow(2).sum()
     
     def backward(self, x, t):
         #This is dl_dx. (dloss wrt to x for MSE loss is 2(x-t))
         t = convert_to_one_hot_labels(torch.tensor([0, 1]), t)
-        return 2 * (x-t)
+        return 2 * (x - t)
     # MAYBEWRONG UPDATE THIS LATER
+    
         
-#class Tanh(Module):
-#    def __init__(self):
-#        super(Tanh, self).__init__()
-#        
-#    def forward(self,x):
-#        return x.tanh()
-#    
-#    def backward(self,s,dl_dx):
-#        """ dtanh = 1/cosh^2"""
-#        return dl_dx* (1/(torch.pow(torch.cosh(s),2)))   
-#               
-#    
-#class CrossEntropyLoss(Module):        
-#    def __init__(self):
-#        Module.__init__(self)
-#        
-#    def softmax(x):
-#        """
-#            Computes softmax with shift to be numerically stable for
-#            large numbers or floats takes exp(x-max(x)) instead of exp(x)
-#        """
-#            #this is really stablesoftmax(x)
-#            #rather than softamx(x)
-#        z = x- x.max()
-#        exps = torch.exp(z)
-#        return (exps/torch.sum(exps))
-#    
-#    def forward(self,x,t):
-#        """
-#            With pj = exp(aj)/sum(exp(ak))
-#            Loss = -Sum_j (yj) log(pj), 
-#            with t the target being the y in the formula
-#            and pj = softmax(x)_j
-#            log(p)*t does the element-wise product then we sum
-#        """
-#        p = self.softmax(x)
-#        sumResult = -torch.sum(torch.log(p)*t)
-#        return sumResult
-#    
-#    def backward(self,x,t):
-#        """
-#            computes dLoss 
-#            dl/dx_i = pi-yi from the slides
-#        """
-#        p = self.softmax(x)
-#        return p-t
+class Tanh(Module):
+    def __init__(self):
+        super(Tanh, self).__init__()
+        
+    def forward(self,x):
+        return x.tanh()
+    
+    def backward(self,s,dl_dx):
+        """ dtanh = 1/cosh^2"""
+        return dl_dx* (1/(torch.pow(torch.cosh(s),2)))   
+               
+    
+class CrossEntropyLoss(Module):        
+    def __init__(self):
+        Module.__init__(self)
+        
+    def softmax(self,x):
+        """
+            Computes softmax with shift to be numerically stable for
+            large numbers or floats takes exp(x-max(x)) instead of exp(x)
+        """
+            #this is really stablesoftmax(x)
+            #rather than softamx(x)
+        z = x-x.max()
+        exps = torch.exp(z)
+        return (exps/torch.sum(exps))
+    
+    def forward(self,x,t):
+        """
+            With pj = exp(aj)/sum(exp(ak))
+            Loss = -Sum_j (yj) log(pj), 
+            with t the target being the y in the formula
+            and pj = softmax(x)_j
+            log(p)*t does the element-wise product then we sum
+        """
+        p = self.softmax(x)
+        t = convert_to_one_hot_labels(torch.tensor([0, 1]), t)
+        sumResult = -torch.sum(torch.log(p)*t)
+        return sumResult
+    
+    def backward(self,x,t):
+        """
+            computes dLoss 
+            dl/dx_i = pi-yi from the slides
+        """
+        p = self.softmax(x)
+        t = convert_to_one_hot_labels(torch.tensor([0, 1]), t)
+        return p-t
 
 def convert_to_one_hot_labels(input, target):
     tmp = input.new_zeros(target.size(0), target.max() + 1)
--- helpers.py
+++ helpers.py
@@ -11,12 +11,59 @@ def plot_data(data, labels):
     
     plt.scatter(data_out[:,0],data_out[:,1])
     plt.scatter(data_in[:,0], data_in[:,1])
-
-def generate_disc_set(nb_sample):
+    plt.show()
+    
+def generate_disc_set(nb_sample, show_data=False):
     
     data = torch.empty(nb_sample, 2).uniform_(0, 1)
     labels = data.sub(0.5).pow(2).sum(1).sub(1/(2*math.pi)).sign().add(1).div(2).long()
     
-    plot_data(data, labels)
+    data_test = torch.empty(nb_sample, 2).uniform_(0, 1)
+    labels_test = data_test.sub(0.5).pow(2).sum(1).sub(1/(2*math.pi)).sign().add(1).div(2).long()
+    
+    if show_data:
+        plot_data(data, labels)
 
-    return data, labels
+    return data, labels, data_test, labels_test
+
+def normalize_data(data, data_test):
+    mean, std = data.mean(), data.std()
+
+    data.sub_(mean).div_(std)
+    data_test.sub_(mean).div_(std)
+    return data, data_test
+
+def train_model(model, criterion, optimizer, data, target, nb_epochs, mini_batch_size):
+    losses = []
+
+    for e in range(nb_epochs):
+        sum_loss = 0
+        for b in range(0, data.size(0), mini_batch_size):
+            output = model(data.narrow(0, b, mini_batch_size))
+            
+            loss = criterion.forward(output,target.narrow(0, b, mini_batch_size))
+            sum_loss += loss.item()
+            
+            model.zero_grad()
+            
+            dl_dx = criterion.backward(output,target.narrow(0, b, mini_batch_size))
+            model.backward(dl_dx)
+            
+            optimizer.step()
+            
+        losses.append(sum_loss)
+    return losses
+
+
+def compute_nb_errors(model, data_input, data_target, mini_batch_size):
+
+    nb_data_errors = 0
+
+    for b in range(0, data_input.size(0), mini_batch_size):
+        output = model(data_input.narrow(0, b, mini_batch_size))
+        _, predicted_classes = torch.max(output, 1)
+        for k in range(mini_batch_size):
+            if data_target[b + k] != predicted_classes[k]:
+                nb_data_errors = nb_data_errors + 1
+
+    return nb_data_errors
